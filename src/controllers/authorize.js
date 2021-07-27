a7811f433c0441c7bc34f0edebe02b2ed25e0e6f
Restyled.io <commits@restyled.io>
2021-07-27 08:35:50 +0000

    Restyled by clang-format

--- src/controllers/authorize.js
+++ src/controllers/authorize.js
@@ -4,16 +4,13 @@ const jwt = require("jsonwebtoken");
 
 exports.create = async (req, res, next) => {
   try {
-    const { body } = req;
-    const token = jwt.sign(
-      {
-        username: body.username,
-        password: body.password,
-      },
-      process.env.JWT_KEY,
-      { expiresIn: "1h" }
-    );
-    return res.status(200).send({ token });
+    const {body} = req;
+    const token = jwt.sign({
+      username : body.username,
+      password : body.password,
+    },
+                           process.env.JWT_KEY, {expiresIn : "1h"});
+    return res.status(200).send({token});
   } catch (error) {
     next(error);
   }
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 exports.token = (req, res, next) => {
-    try {
-        const decode = jwt.verify(req.headers.authorization, process.env.JWT_KEY);
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Unauthorized' })
-    }
+  try {
+    const decode = jwt.verify(req.headers.authorization, process.env.JWT_KEY);
+    next();
+  } catch (error) {
+    return res.status(401).json({message : 'Unauthorized'})
+  }
 }
