6a7d86a4fff27dc3b8e219cc1cb8d6098e0d32d4
Restyled.io <commits@restyled.io>
2021-07-27 08:35:55 +0000

    Restyled by prettier

--- src/controllers/authorize.js
+++ src/controllers/authorize.js
@@ -4,13 +4,16 @@ const jwt = require("jsonwebtoken");
 
 exports.create = async (req, res, next) => {
   try {
-    const {body} = req;
-    const token = jwt.sign({
-      username : body.username,
-      password : body.password,
-    },
-                           process.env.JWT_KEY, {expiresIn : "1h"});
-    return res.status(200).send({token});
+    const { body } = req;
+    const token = jwt.sign(
+      {
+        username: body.username,
+        password: body.password,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+    return res.status(200).send({ token });
   } catch (error) {
     next(error);
   }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {celebrate, Joi, errors, Segments} = require("celebrate");
+const { celebrate, Joi, errors, Segments } = require("celebrate");
 const routes = express.Router();
 const Students = require("./controllers/students");
 const Authorize = require("./controllers/authorize");
@@ -8,45 +8,62 @@ const AuthMiddleware = require("./middlewares/auth");
 routes.get("/", async (req, res, next) => {
   try {
     res.send({
-      uptime : process.uptime(),
-      message : "OK",
-      timestamp : new Date(Date.now()).toISOString(),
+      uptime: process.uptime(),
+      message: "OK",
+      timestamp: new Date(Date.now()).toISOString(),
     });
   } catch (e) {
     res.status(503).send();
   }
 });
 
-routes.post("/v1/api-ed-tech/authorize", celebrate({
-              [Segments.BODY] : Joi.object({
-                username : Joi.string().max(50).required(),
-                password : Joi.string().max(50).required(),
-              }),
-            }),
-            Authorize.create);
+routes.post(
+  "/v1/api-ed-tech/authorize",
+  celebrate({
+    [Segments.BODY]: Joi.object({
+      username: Joi.string().max(50).required(),
+      password: Joi.string().max(50).required(),
+    }),
+  }),
+  Authorize.create
+);
 
 routes
-    .post("/v1/api-ed-tech/students", AuthMiddleware.token, celebrate({
-            [Segments.BODY] : Joi.object({
-              name : Joi.string().max(100).required(),
-              email : Joi.string().email().max(256).required(),
-              ra : Joi.number().min(1).max(999999999).required(),
-              cpf : Joi.string().regex(/^\d{11}$/).max(11).required(),
-            }),
-          }),
-          Students.create)
-    .get("/v1/api-ed-tech/students", AuthMiddleware.token, Students.getAll)
-    .get("/v1/api-ed-tech/students/:id", AuthMiddleware.token, Students.getByID)
-    .patch("/v1/api-ed-tech/students/:id", AuthMiddleware.token, celebrate({
-             [Segments.BODY] : Joi.object({
-               name : Joi.string().max(100),
-               email : Joi.string().email().max(256),
-               active : Joi.boolean(),
-             }),
-           }),
-           Students.update)
-    .delete("/v1/api-ed-tech/students/:id", AuthMiddleware.token,
-            Students.delete);
+  .post(
+    "/v1/api-ed-tech/students",
+    AuthMiddleware.token,
+    celebrate({
+      [Segments.BODY]: Joi.object({
+        name: Joi.string().max(100).required(),
+        email: Joi.string().email().max(256).required(),
+        ra: Joi.number().min(1).max(999999999).required(),
+        cpf: Joi.string()
+          .regex(/^\d{11}$/)
+          .max(11)
+          .required(),
+      }),
+    }),
+    Students.create
+  )
+  .get("/v1/api-ed-tech/students", AuthMiddleware.token, Students.getAll)
+  .get("/v1/api-ed-tech/students/:id", AuthMiddleware.token, Students.getByID)
+  .patch(
+    "/v1/api-ed-tech/students/:id",
+    AuthMiddleware.token,
+    celebrate({
+      [Segments.BODY]: Joi.object({
+        name: Joi.string().max(100),
+        email: Joi.string().email().max(256),
+        active: Joi.boolean(),
+      }),
+    }),
+    Students.update
+  )
+  .delete(
+    "/v1/api-ed-tech/students/:id",
+    AuthMiddleware.token,
+    Students.delete
+  );
 
 module.exports = {
   routes,
